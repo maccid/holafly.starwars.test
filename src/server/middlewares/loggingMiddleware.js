a81ffe7fece49b4f4b39e122d5c0807f16bd57a6
Miguel Ángel Cid García <mac6656@gmail.com>
2024-01-17 21:12:00 +0100

    feat(createLog): Añadida funcionalidad en loggingMiddleware para guardar ip, action y el header de cada petición que se realice.

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -3,7 +3,13 @@ const loggingMiddleware = (db) =>
         const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
         const headers = JSON.stringify(req.headers);
         const originalUrl = req.originalUrl;
-        // Persist this info on DB
+
+        db.logging.create({
+            ip,
+            action: originalUrl,
+            header: headers,
+        });
+
         next();
     }
 
